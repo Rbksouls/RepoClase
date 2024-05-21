ac8cf88dced8aeb7c9765fbcca71b7e4554b5116
rbksouls <pabloripense@gmail.com>
2024-05-21 08:57:46 +0200

    todo instalado

---
+++ Nuxt/my-project/pages/contacto.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Hola a todos!!</h1>
+      <p>Soy la página de contacto...</p>
+    </div>
+    <NuxtLink to="/">Ir al inicio</NuxtLink>
+</template>
---
+++ Nuxt/my-project/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <NuxtLink to="/contacto">Ir a la página de contacto</NuxtLink>
+</template>
+
